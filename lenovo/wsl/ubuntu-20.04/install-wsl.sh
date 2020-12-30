ee598e86d68b3891df09c769f8e7593790afedc0
WillCallahan <willcallahan94@att.net>
2020-12-29 22:44:12 -0500

    Added lenovo configuration

---
+++ lenovo/wsl/ubuntu-20.04/install-wsl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo sed -i 's/deb http.*\/\s/deb https:\/\/nexus.i.callahanwilliam.com\/repository\/apt-proxy\/ /g' /etc/apt/sources.list
+ex +'/BEGIN CERTIFICATE/,/END CERTIFICATE/p' <(echo | openssl s_client -showcerts -connect nexus.i.callahanwilliam.com:443) -scq > nexus.i.callahanwilliam.com.crt
+sudo mv nexus.i.callahanwilliam.com.crt /usr/local/share/ca-certificates/
+sudo update-ca-certificates
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt update
+sudo apt install -y zsh fzf ripgrep silversearcher-ag npm lftp ffmpeg python3 python3-venv openjdk-14-jdk neovim iperf awscli subversion
+cd ~
+mkdir git
+cd git
+git clone https://github.com/Lugoues/dot-zsh.git
+ls -s /home/${USER}/git/dot-zsh/zsh /home/${USER}/.zsh
+ls -s /home/${USER}/git/dot-zsh/zshrc /home/${USER}/.zshrc
