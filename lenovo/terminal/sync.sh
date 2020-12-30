67dedc0ae36c3953793e79ef35dbbcf3bef05352
WillCallahan <willcallahan94@att.net>
2020-12-29 22:46:42 -0500

    Added window terminal sync script

---
+++ lenovo/terminal/sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Syncing Windows Terminal Settings..."
+cp /c/Users/willc/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json settings.json
+echo "Settings Synced!"
