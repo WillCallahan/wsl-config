0d3444660ef42ffeaff75a30e8ad0ad94ec6f2d0
WillCallahan <willcallahan94@att.net>
2020-12-29 23:12:48 -0500

    Added Icons

--- lenovo/terminal/sync.sh
+++ lenovo/terminal/sync.sh
@@ -2,4 +2,7 @@
 
 echo "Syncing Windows Terminal Settings..."
 cp /c/Users/willc/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json settings.json
+echo "Syncing Icons..."
+cp *.png /c/Users/willc/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
+cp *.ico /c/Users/willc/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
 echo "Settings Synced!"
