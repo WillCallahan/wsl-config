14a9707353d8bc55d971fdbc153aa4d1da183b5a
WillCallahan <willcallahan94@att.net>
2021-01-02 15:02:46 -0500

    Added installation steps for sqlcmd

--- lenovo/wsl/ubuntu-20.04/setup.sh
+++ lenovo/wsl/ubuntu-20.04/setup.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+__dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 sudo sed -i 's/deb http.*\/\s/deb https:\/\/nexus.i.callahanwilliam.com\/repository\/apt-proxy\/ /g' /etc/apt/sources.list
 ex +'/BEGIN CERTIFICATE/,/END CERTIFICATE/p' <(echo | openssl s_client -showcerts -connect nexus.i.callahanwilliam.com:443) -scq > nexus.i.callahanwilliam.com.crt
 sudo mv nexus.i.callahanwilliam.com.crt /usr/local/share/ca-certificates/
 sudo update-ca-certificates
 sudo rm -rf /var/lib/apt/lists/*
 sudo apt update
-sudo apt install -y zsh fzf ripgrep silversearcher-ag npm lftp ffmpeg python3 python3-venv openjdk-14-jdk neovim iperf awscli subversion jq lastpass-cli
+sudo apt install -y zsh fzf ripgrep silversearcher-ag npm lftp ffmpeg python3 python3-venv openjdk-14-jdk neovim iperf awscli subversion jq lastpass-cli pandoc postgresql-client mysql-client
+
+echo "Installing yq"
+wget https://github.com/mikefarah/yq/releases/download/v4.2.1/yq_linux_amd64 -O yq && chmod +x ~/yq
+sudo mv ~/yq /usr/bin/yq
+
+echo "Installing mssql-client"
+cat "${__dir}/microsoft.asc" | sudo apt-key add -
+echo 'deb [arch=amd64,armhf,arm64] https://nexus.i.callahanwilliam.com/repository/apt-proxy-microsoft focal main' | sudo tee /etc/apt/sources.list.d/msprod.list
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.zshrc
+
 cd ~
 mkdir git
 cd git
